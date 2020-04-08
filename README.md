3e1c2fe35783045666f76371a69e19d8cc836424
Peter Morrison <bwt.bluewebtech@gmail.com>
2020-04-08 07:49:33 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# 99 Bottles of Beer on the Wall
+
+99 Bottles of Beer on the Wall in 99 programming languages.
+
+## Languages
+
+1. [PHP](https://tio.run/##tVbLbqMwFN3nK66iSkAk0qTtpmGaVjOL@YDJcqSKgFM8dW1km0Sdab49Y5uHeQSQWsUSQsbnPs69xzZpkp5O3x7TJJ1MIhIKAd@ZlAQJ@DcBNa5nM/OGGWwSBFHGOaIStgbkCg8YBakWDiEh8wJZGjztQw6YymJ@bd4pZxJFEsVwlTuBB1gEk3PBJJMhKUKJrwcS/ZFy5hGjQvIskox3IqQhD990jMpbM1i2JTiCXUYjiVWmz8@VM7c08AwyL6seVzLBwl/b5EpgYCDHTqY/EhS9At7pOnAEoX7oe1UggSWmL4N14khmnCoLRnpqVTFIQlEowfVW1sKmX/hyWzTWsPD6CPxE0uSWkoyHBP9V8VSNTNKGVEteg1Q6DWmTzD2P0axScWuuFOGadYeyk6McmEPZXEV7CR8fVtNKavAIjnBgBY7TW5FfmrzmKBh96WvXnuF4UG1CedFdqoA2ZaWWskP1jno1iB47xi2Xh5Y0A7AsF9XE99tOOqK2mg7OAztf9fDXe8QFWrre0PLN8PLt8PLdmeVmjsdqdgREBGpxRVHCYLqpNmKhXPIOlJ09sqbW/XFke@wwFxJMnuXGGJIH2/5Roh4TelnTVR1vORk@zebNwQGt8eKr3SkNmKcQU53mFiFeZ/ybTovDtrZ1jOnY8SCQOjzjixTg5sIF@DxpheGX4Xw7zHm6CV@ROr1idqDqPokh1XchlkrWLKPx5xntWMZlchFKd8OUmg0CH5ZefzPPgAc0PV4OVZDaDwdFh/KPyr2/V/dAueav81M7mJxO/wE)
+2. JavaScript
+3. TypeScript
+4. Dart
+5. CoffeeScript
+6. Python
+7. Bash
+8. Go
+9. Rust
+10. Julia
+11. C
+12. C++
+13. C#
+14. Visual Basic
+15. Java
+16. Groovy
+17. Kotlin
+18. Haskell
+19. Lua
+20. Pascal
+21. Perl
+22. R
+23. Ruby
+24. Scala
+25. Swift
+26. Brainfuck
+27. COBOL
+28. Clojure
+29. Fortran
+30. Emojicode
---
+++ php.php
@@ -0,0 +1,121 @@
+<?php
+
+class Bottles {
+    /**
+     * The current bottle(s) on the wall.
+     *
+     * @var int
+     */
+    protected $bottle = 0;
+
+    /**
+     * The total bottles on the wall.
+     *
+     * @var int
+     */
+    protected $bottles = 0;
+
+    /**
+     * The class constructor.
+     *
+     * @param int $bottles
+     */
+    public function __construct($bottles)
+    {
+        $this->bottles = $bottles;
+    }
+
+    /**
+     * Check if there are any bottles siting on the wall.
+     *
+     * @return bool
+     */
+    protected function hasBottles(): bool
+    {
+        return ($this->bottles > 0);
+    }
+
+    /**
+     * Get the pluralized string of the current bottle on the wall.
+     *
+     * @param int $bottle
+     * @return string
+     */
+    protected function pluralize(int $bottle): string
+    {
+        return 'bottle' . ($bottle > 1 || $bottle == 0 ? 's' : '');
+    }
+
+    /**
+     * Sing the song.
+     *
+     * @return void
+     */
+    public function sing(): void
+    {
+        if ($this->hasBottles()) {
+            for ($bottle = $this->bottles; $bottle > 0; $bottle--) {
+                $this->bottle = $bottle;
+                $this
+                    ->verse1()
+                    ->verse2()
+                    ->verse3()
+                    ->verse4()
+                ;
+            }
+        } else {
+            echo "There are currently no bottles on the wall.";
+        }
+    }
+
+    /**
+     * Get the first verse of the song.
+     *
+     * @return object
+     */
+    protected function verse1(): object
+    {
+        echo $this->bottle . ' ' . $this->pluralize($this->bottle) . " of beer on the wall\n";
+
+        return $this;
+    }
+
+    /**
+     * Get the second verse of the song.
+     *
+     * @return object
+     */
+    protected function verse2(): object
+    {
+        echo $this->bottle . ' ' . $this->pluralize($this->bottle) . " of beer\n";
+
+        return $this;
+    }
+
+    /**
+     * Get the third verse of the song.
+     *
+     * @return object
+     */
+    protected function verse3(): object
+    {
+        echo "Take 1 down and pass it around\n";
+
+        return $this;
+    }
+
+    /**
+     * Get the fourth verse of the song.
+     *
+     * @return object
+     */
+    protected function verse4(): object
+    {
+        echo ($this->bottle - 1) . ' ' . $this->pluralize(($this->bottle - 1)) . " of beer on the wall\n\n";
+
+        return $this;
+    }
+}
+
+$bottles = new Bottles(99);
+$bottles->sing();
